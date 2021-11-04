eefadc552ac607384441279c652c9b1ab93499d1
Anwar Hossain <anwar.sarker@bjitgroup.com>
2021-11-04 09:01:52 +0600

    Handling Functions

--- src/components/List.tsx
+++ src/components/List.tsx
@@ -1,16 +1,26 @@
 import React from "react";
+import { IState as Props } from "../App";
 
 interface IProps {
-  people: {
-    name: string;
-    age: number;
-    img: string;
-    note?: string;
-  }[];
+  people: Props["people"];
 }
 
 const List: React.FC<IProps> = ({ people }) => {
-  return <div>iam list</div>;
+  const renderList = (): JSX.Element[] => {
+    return people.map((person, i) => {
+      return (
+        <li className="List" key={i}>
+          <div className="List-header">
+            <img className="List-img" src={person.img} alt="user" />
+            <h2>{person.name}</h2>
+          </div>
+          <p>{person.age} years old</p>
+          <p className="List-note">{person.note}</p>
+        </li>
+      );
+    });
+  };
+  return <ul>{renderList()}</ul>;
 };
 
 export default List;
